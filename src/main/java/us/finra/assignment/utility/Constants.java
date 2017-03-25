0750000cd4faae83525287a08c3a9df9e3354ed7
anna-217 <anna.zhang217@gmail.com>
2017-03-24 23:03:27 -0400

    second commit

--- src/main/java/us/finra/assignment/utility/Constants.java
+++ src/main/java/us/finra/assignment/utility/Constants.java
@@ -7,6 +7,6 @@ public class Constants {
 	public final String FILE_SYSTEM_PATH = "E:\\finra files\\";
 	public final Long ONE_HOUR_IN_MS = (long) 5000; // 5sc
 	
-	public final String TO_EMAIL_ADD = "chen.zhang91@yahoo.com";
-	public final String FROM_EMAIL_ADD = "anna.zhang217@gmail.com";
+	public final String TO_EMAIL_ADD = "xxx@xxx";
+	public final String FROM_EMAIL_ADD = "xxxx";
 }
